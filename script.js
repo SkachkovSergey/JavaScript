615b7ca53526762eba2bbd2fc08f0164dd9ecd51
Sergey Skachkov <sergey.skatchkov@gmail.com>
2024-02-13 23:16:53 +0100

    ternary operator

--- script.js
+++
@@ -1,7 +0,0 @@
-/*let code = prompt('Vul uw promotiecode in', 'uw code');
-let text = 'De code die je invoerde was: ' + code;
-document.getElementById('kortingscode').innerHTML = text;*/
-console.log('Demo debugging JS');
-let text = 'Sergey Skachkov';
-console.log(text);
-console.log(typeof text);
---
+++ voorwaardelijkeOperator/script.js
@@ -0,0 +1,4 @@
+let dag = 'Maandag';
+let uitspraak;
+uitspraak = (dag === 'Dinsdag')? 'Joepie, feest!': 'Geen feest.';
+console.log(uitspraak);
