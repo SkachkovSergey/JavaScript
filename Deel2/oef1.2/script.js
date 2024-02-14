55ca2b4d1c71a81c51ea9d6c7a89cb9f91825bb7
Sergey Skachkov <sergey.skatchkov@gmail.com>
2024-02-14 13:19:30 +0100

    oef 1.2

---
+++ Deel2/oef1.2/script.js
@@ -0,0 +1,5 @@
+let zin1 = "Dit is de eerste zin.";
+let zin2 = "Dit is de tweede zin.";
+let zin3 = "Dit is de derde zin.";
+alert(zin1 + ' ' + zin2 + ' ' + zin3);
+console.log(zin1 + ' ' + zin2 + ' ' + zin3);
