f6e16e3cd4c35cf509f42d80edd024a8f47a3414
Sergey Skachkov <sergey.skatchkov@gmail.com>
2024-02-15 11:17:22 +0100

    10

---
+++ Deel2/oef1.10/script.js
@@ -0,0 +1,4 @@
+let leeftijd = parseInt(prompt('Geef je leeftijd.'));
+let boodschap = (leeftijd >= 18)? 'Welcome to my page!':
+    'Helaas... Probeer maar de volgende editie maar opnieuw.';
+alert(boodschap);
