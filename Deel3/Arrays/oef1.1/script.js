76e5c5554a315f73df6d7f25e3100d32c6e429c9
Sergey Skachkov <sergey.skatchkov@gmail.com>
2024-02-22 19:31:13 +0100

    oef1.1

---
+++ Deel3/Arrays/oef1.1/script.js
@@ -0,0 +1,5 @@
+const opleidingen = ['Electronica-ICT', 'Toegepaste Informatica',
+    'Programmeren', 'Digitale vormgeving', 'Internet en things',
+    'System- en Netwerkbeheer'];
+
+console.log('In department Digital zijn er ' + opleidingen.length + ' opleidingen.');
