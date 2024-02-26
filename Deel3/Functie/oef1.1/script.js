d89fc00c54605bc721a216351e787fff15b29225
Sergey Skachkov <sergey.skatchkov@gmail.com>
2024-02-26 09:44:44 +0100

    oef1.1

---
+++ Deel3/Functie/oef1.1/script.js
@@ -0,0 +1,6 @@
+function Vermenigvuldig(getal1, getal2) {
+    document.getElementById('output').innerHTML = String(getal1 * getal2);
+}
+let firstNumber = 5;
+let secondNumber = 4;
+Vermenigvuldig(firstNumber, secondNumber);
