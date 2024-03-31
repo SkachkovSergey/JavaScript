2c6c9fb16437446937e160446b31581d26720fbf
Sergey Skachkov <sergey.skatchkov@gmail.com>
2024-03-31 14:01:48 +0200

    oef1.4fullVersion

---
+++ Deel5/NewVariantOef1.4/script.js
@@ -0,0 +1,42 @@
+const firstPara = document.getElementsByTagName('p')[0];
+const secondPara = document.getElementsByTagName('p')[1];
+const thirdPara = document.getElementsByTagName('p')[2];
+
+let firstText = document.createTextNode('Om de tekst beter te zien kun je deze vergroten door op de knop "Groter" te klikken.');
+let secondText = document.createTextNode('De knop "Kleiner" maakt de font size kleiner. Zo bespaart u een ruimte in de website!');
+let thirdText = document.createTextNode('Kies wat je best past!');
+
+firstPara.appendChild(firstText);
+secondPara.appendChild(secondText);
+thirdPara.appendChild(thirdText);
+
+document.body.insertBefore(document.getElementsByTagName('h1')[0], firstPara);
+document.body.insertBefore(document.getElementsByTagName('p')[0], secondPara);
+document.body.insertBefore(document.getElementsByTagName('p')[1], thirdPara);
+
+document.getElementsByTagName('button')[0].addEventListener('click', textGroter);
+document.querySelectorAll('button')[1].addEventListener('click', textKleiner);
+
+function textGroter() {
+    let aantalParagrafen = document.getElementsByTagName('p').length;
+    let aantalHeadings = document.getElementsByTagName('h1').length;
+
+    for(let teller = 0; teller < aantalParagrafen; teller++) {
+        document.getElementsByTagName('p')[teller].style.fontSize = '24px';
+    }
+    for(let teller = 0; teller < aantalHeadings; teller++) {
+        document.getElementsByTagName('h1')[teller].style.fontSize = '48px';
+    }
+}
+
+function textKleiner() {
+    let aantalParagrafen = document.getElementsByTagName('p').length;
+    let aantalHeadings = document.getElementsByTagName('h1').length;
+
+    for(let teller = 0; teller < aantalParagrafen; teller++) {
+        document.getElementsByTagName('p')[teller].style.fontSize = '14px';
+    }
+    for(let teller = 0; teller < aantalHeadings; teller++) {
+        document.getElementsByTagName('h1')[teller].style.fontSize = '22px';
+    }
+}
--- Deel5/reuseOef1.3/script.js
+++ Deel5/reuseOef1.3/script.js
@@ -3,9 +3,15 @@ let teller = 0;
 document.getElementsByTagName('button')[0].addEventListener('click', addSentence);
 document.getElementsByTagName('button')[1].addEventListener('click', wissenAll);
 
-let buttons = document.getElementById('addButton');
-buttons.style.background = '#0000f0';
-buttons.style.color = '#ffffff';
+let addButton = document.getElementById('addButton');
+addButton.style.background = '#0000f0';
+addButton.style.color = '#ffffff';
+addButton.style.borderRadius = '5px';
+
+const wissenButton = document.getElementsByTagName('button')[1];
+wissenButton.style.background = '#ff0000';
+wissenButton.style.color = '#ffffff';
+wissenButton.style.borderRadius = '5px';
 
 function addSentence() {
    teller++;
@@ -14,6 +20,12 @@ function addSentence() {
    paragraaf.appendChild(paragraafText);
    paragraaf.id = 'p' + teller;
    document.getElementsByTagName('body')[0].appendChild(paragraaf);
+
+    let items = document.querySelectorAll('p');// een font size vergroten
+    for(let p of items) {
+        p.style.fontSize = '24px';
+    }
+
    if(teller === 1) {
        document.getElementById('p1').style.color = '#ff0000';
        document.getElementById('p1').style.fontWeight = 'bold';
@@ -26,16 +38,16 @@ function addSentence() {
    }
 }
 function wissenAll() {
-    // if(teller >= 1) {
-    //     for(let count = 1; count <= teller; count++) {
-    //         let item = 'p' + count;
-    //         document.getElementsByTagName('body')[0].removeChild(document.getElementById(item));
-    //     }
-    // }
-    let paragrafen = document.querySelectorAll('p');
-
-    for(let para of paragrafen) {
-        para.remove();
+    if(teller >= 1) {
+        for(let count = 1; count <= teller; count++) {
+            let item = 'p' + count;
+            document.getElementsByTagName('body')[0].removeChild(document.getElementById(item));
+        }
     }
+    // let paragrafen = document.querySelectorAll('p');
+    //
+    // for(let para of paragrafen) {
+    //     para.remove();
+    // }
     teller = 0;
 }
